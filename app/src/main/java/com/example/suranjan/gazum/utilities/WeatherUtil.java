b910e7d99551006738b91ee4466b0dbd49022739
Suranjan77 <7suranjanpoudel7@gmail.com>
2018-10-28 20:36:07 +0545

    Delete WeatherUtil.java

--- app/src/main/java/com/example/suranjan/gazum/utilities/WeatherUtil.java
+++
@@ -1,6 +0,0 @@
-package com.example.suranjan.gazum.utilities;
-
-public class WeatherUtil {
-    public static final String KEY = "6839747f87dc183d8d557cec8ae099c2";
-    public static final String BASE_URL = "https://api.darksky.net/forecast/";
-}
